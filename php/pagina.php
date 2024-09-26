052e54eef1b03268515196dfab69a60bf4428154
Auto Logon <autologon@CSED.LAB>
2024-09-25 21:16:04 -0300

    inserindo informações sobre PHP

--- php/pagina.php
+++ php/pagina.php
@@ -9,7 +9,31 @@ O que o PHP pode fazer?
 - O PHP pode enviar e receber cookies
 - O PHP pode adicionar, excluir, modificar dados em seu banco de dados
 - PHP pode ser usado para controlar o acesso do usuário
-- O PHP pode criptografar dados*/
+- O PHP pode criptografar dados
+
+O que eu preciso?
+
+Para começar a usar o PHP, você pode:
+
+Encontre um host com suporte a PHP e MySQL
+Instale um servidor web em seu próprio PC e, em seguida, instale o PHP e o MySQL
+Use um host com suporte a PHP
+Se o seu servidor ativou o suporte para PHP, você não precisa fazer qualquer coisa.
+
+Basta criar alguns arquivos, colocá-los em seu diretório web e no servidor irá analisá-los automaticamente para você..php
+
+Você não precisa compilar nada ou instalar ferramentas extras.
+
+Como o PHP é gratuito, a maioria dos hosts da web oferecem suporte a PHP.
+
+Configure o PHP em seu próprio PC
+No entanto, se o seu servidor não suportar PHP, você deverá:
+
+Instalar um servidor web
+instalar o PHP
+instalar um banco de dados, como o MySQL
+O site oficial do PHP (PHP.net) tem instruções de instalação para PHP: http://php.net/manual/en/install.php
+*/
     echo "Qualé rapaziada!!!!!!!"; 
 
 ?>
