674a9a47039c09d1dc4c711c8e7eff74472bd46e
Auto Logon <autologon@CSED.LAB>
2024-09-25 21:03:28 -0300

    aprendendo PHP

---
+++ php/pagina.php
@@ -0,0 +1,15 @@
+<?php
+/*aprendendo sobre o php 
+
+O que o PHP pode fazer?
+
+- PHP pode gerar conteúdo de página dinâmico
+- O PHP pode criar, abrir, ler, gravar, excluir e fechar arquivos no servidor
+- O PHP pode coletar dados de formulário
+- O PHP pode enviar e receber cookies
+- O PHP pode adicionar, excluir, modificar dados em seu banco de dados
+- PHP pode ser usado para controlar o acesso do usuário
+- O PHP pode criptografar dados*/
+    echo "Qualé rapaziada!!!!!!!"; 
+
+?>
